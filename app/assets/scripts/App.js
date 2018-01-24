addf0201a4e11ae80e0124482cf30bf812b58ee8
Lawrence Caulker <locaulker@gmail.com>
2018-01-23 19:57:06 -0500

    installed and configured babel

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,15 +1,25 @@
 var $ = require('jquery');
-var Person = require('./modules/Person');
+
+// var Person = require('./modules/Person'); // ES5 way
+import Person from './modules/Person';
 
 // alert("This is a test for our webpack automation!");
 // alert("Testing 123");
-alert("ABC 321");
+// alert("ABC 321");
+
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes $0 in taxes.");
+  }
+}
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
+// var jane = new Adult("Jane Smith", "green");
+var jane = new Adult("Jane Smith", "orange");
 jane.greet();
+jane.payTaxes();
 
 
 $("h1").css('color','green');
